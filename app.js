ca60c913e5469cb428df236fd50901f0fdeeb323
Papp Kornel <ykornely@gmail.com>
2020-10-16 11:21:29 +0200

    body parser and cors package (middleware)  added, express server starter added

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 var app = express();
+
+//middleware
+// bodyparser is needed so we can pass json datas into this app
+app.use(bodyParser.json());
+// this node.js app and the client-side app will be running in two different port numbers, 
+// so to communicate between them, we need to add the cors package
+app.use(cors());
+
+// start express server
+// we pass the port number with process.env.PORT
+app.listen(process.env.PORT, () => console.log(`Server started at port: ${process.env.PORT}`));
